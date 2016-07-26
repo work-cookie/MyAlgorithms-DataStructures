483ed63490a552becd6e0d525a475a75625e5b0a
work-cookie <chandrasekhar.challa435@gmail.com>
2016-07-26 22:57:43 +0530

    Testing Queue implementation using Arrays/LinkedList

--- test/com/solutions/datastructures/queues/ArrayQueueTest.java
+++ test/com/solutions/datastructures/queues/ArrayQueueTest.java
@@ -2,7 +2,10 @@ package test.com.solutions.datastructures.queues;
 
 import com.solutions.datastructures.queues.ArrayQueue;
 import com.solutions.datastructures.queues.Queue;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -11,28 +14,66 @@ import static org.junit.Assert.*;
  */
 public class ArrayQueueTest {
 
+    Queue<Integer> q;
+
+    @Before
+    public void before(){
+        System.out.println("Before");
+        q = new ArrayQueue<Integer>();
+        q.enQueue(10);
+        q.enQueue(30);
+        q.enQueue(70);
+    }
+
     @org.junit.Test
     public void testEnQueue() throws Exception {
-        Queue<Integer> aq = new ArrayQueue<Integer>();
-        aq.enQueue(10);
-        aq.enQueue(30);
-        aq.enQueue(70);
-        Assert.assertEquals(3,aq.size());
-        Assert.assertEquals("10\t30\t\70\t",aq.toString());
+        Assert.assertEquals(3,q.size());
+        Assert.assertEquals("10\t30\t70",q.toString().trim());
     }
-
     @org.junit.Test
     public void testDeQueue() throws Exception {
-
+        q.deQueue();
+        System.out.println(q.toString());
+        q.deQueue();
+        Assert.assertEquals(1,q.size());
+        Assert.assertEquals("70",q.toString().trim());
     }
-
     @org.junit.Test
     public void testPeek() throws Exception {
-
+        System.out.println(q.toString());
+        q.enQueue(50);
+        Assert.assertEquals(4,q.size());
+        Assert.assertEquals(Integer.valueOf(10),q.peek());
     }
 
     @org.junit.Test
     public void testToString() throws Exception {
 
+    }
+    @After
+    public void after(){
+        q=null;
+    }
+
+    @Test
+    public void main(){
+        Queue<Integer>q = new ArrayQueue<Integer>();
+        q.enQueue(150);
+        q.enQueue(100);
+        q.enQueue(450);
+        q.enQueue(120);
+        q.enQueue(150);
+        q.enQueue(100);
+        q.enQueue(450);
+        q.enQueue(120);
+        q.enQueue(150);
+        q.enQueue(100);
+        q.enQueue(450);
+        q.enQueue(120);
+        System.out.println(q.toString());
+        q.deQueue();
+        q.deQueue();
+        System.out.println("After deletion: "+q.toString());
+        System.out.println("Peek is : "+q.peek());
     }
 }
---
+++ test/com/solutions/datastructures/queues/LinkedQueueTest.java
@@ -0,0 +1,34 @@
+package test.com.solutions.datastructures.queues;
+
+import com.solutions.datastructures.queues.LinkedQueue;
+import com.solutions.datastructures.queues.Queue;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ADMIN-COOLUCAN on 7/26/2016.
+ */
+public class LinkedQueueTest {
+
+    @Test
+    public static void main(String[] args){
+        Queue<Integer> q= new LinkedQueue<Integer>();
+        q.enQueue(10);
+        q.enQueue(50);
+        System.out.println(q);
+        q.enQueue(70);
+        q.enQueue(30);
+        System.out.println(q);
+
+        System.out.println("Removing ele: "+q.deQueue());
+        System.out.println(q);
+        System.out.println("Peek ele: "+q.peek());
+
+        Assert.assertEquals(3,q.size());
+        Assert.assertFalse(q.isEmpty());
+        System.out.println(q);
+    }
+
+}
